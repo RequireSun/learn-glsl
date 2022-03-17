f1358bc2a7902bb7ccd0df5b8f823ac0d2569ba8
RequireSun <kelvinsen@outlook.com>
2022-03-17 21:54:30 +0800

    feat: rename output

--- nlohmann/build.sh
+++ nlohmann/build.sh
@@ -4,7 +4,7 @@ set -xe
 BASEDIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 rm -rf ./build && mkdir ./build && cd ./build
-emcmake cmake -DCMAKE_CXX_FLAGS="-o ${BASEDIR}/build/index.html \
-              -std=c++14" \
+emcmake cmake -DEMSCRIPTEN=YES \
+              -DCMAKE_CXX_FLAGS="-std=c++14" \
               ../src/
 emmake make -j16
