c10056fe297c8f5934cf55634b6b6bbee121abf9
RequireSun <kelvinsen@outlook.com>
2022-03-24 00:17:09 +0800

    feat: no need for extern C

--- nlohmann/src/static-class.h
+++ nlohmann/src/static-class.h
@@ -5,19 +5,17 @@
 #ifndef LEARN_GLSL_STATIC_CLASS_H
 #define LEARN_GLSL_STATIC_CLASS_H
 
-extern "C" {
-  static std::map<std::string, std::string> m_map;
-  class DecryptAlbum {
-  public:
-    static void set(const std::string path, const std::string decrypt_key) {
-      m_map[path] = decrypt_key;
-    }
-    static std::string get(const std::string &path) {
-      auto iter = m_map.find(path);
-      std::string ret = iter == m_map.end() ? "" : iter->second;
-      return ret;
-    }
-  };
+static std::map<std::string, std::string> m_map;
+class DecryptAlbum {
+public:
+  static void set(const std::string path, const std::string decrypt_key) {
+    m_map[path] = decrypt_key;
+  }
+  static std::string get(const std::string &path) {
+    auto iter = m_map.find(path);
+    std::string ret = iter == m_map.end() ? "" : iter->second;
+    return ret;
+  }
 };
 
 #endif //LEARN_GLSL_STATIC_CLASS_H
